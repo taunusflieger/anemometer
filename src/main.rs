4b5374de99fd2a9dc7e2a1d62d76952b472cad17
Michael Zill <michael.zill@gmail.com>
2022-11-11 08:45:11 +0100

    added neopixel based wifi connection status

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,14 @@ use esp_idf_svc::http::server::Configuration;
 use log::info;
 use std::{thread::sleep, time::Duration};
 
+//use core::time::Duration;
 use embedded_svc::wifi::{self, AuthMethod, ClientConfiguration};
+use esp_idf_hal::delay::Ets;
+use esp_idf_hal::delay::FreeRtos;
+use esp_idf_hal::gpio::*;
 use esp_idf_hal::peripherals::Peripherals;
+use esp_idf_hal::rmt::config::TransmitConfig;
+use esp_idf_hal::rmt::*;
 use esp_idf_svc::{
     eventloop::EspSystemEventLoop,
     netif::IpEvent,
@@ -14,6 +20,8 @@ use esp_idf_svc::{
 use esp_idf_sys as _;
 use esp_idf_sys::*;
 use std::sync::mpsc;
+
+use esp_idf_hal::peripheral::*;
 
 use crate::web_server::url_handler;
 mod lazy_http_server;
@@ -32,9 +40,25 @@ enum SysLoopMsg {
     IpAddressAsquired,
 }
 
+enum NeopixelColor {
+    Red = 0x00ff00,
+    Green = 0x0000ff,
+}
+
 fn main() -> anyhow::Result<()> {
     esp_idf_sys::link_patches();
     esp_idf_svc::log::EspLogger::initialize_default();
+
+    // required for neopixel
+    let peripherals = Peripherals::take().unwrap();
+    let mut led = peripherals.pins.gpio18;
+    let mut led_pwr = peripherals.pins.gpio17;
+    let mut led_pwr = PinDriver::output(led_pwr)?;
+    led_pwr.set_high()?;
+    let mut channel = peripherals.rmt.channel0;
+    let config = TransmitConfig::new().clock_divider(1);
+
+    neopixel(NeopixelColor::Red, &mut channel, &config, &mut led)?;
 
     let httpd = lazy_http_server::lazy_init_http_server::LazyInitHttpServer::new();
     let (tx, rx) = mpsc::channel::<SysLoopMsg>();
@@ -49,7 +73,6 @@ fn main() -> anyhow::Result<()> {
         info!("Wifi password is empty");
     }
 
-    let peripherals = Peripherals::take().unwrap();
     let nvs_default_partition = EspDefaultNvsPartition::take()?;
     let sysloop = EspSystemEventLoop::take()?;
 
@@ -110,11 +133,14 @@ fn main() -> anyhow::Result<()> {
         match rx.try_recv() {
             Ok(SysLoopMsg::WifiDisconnect) => {
                 info!("mpsc loop: WifiDisconnect received");
+
+                neopixel(NeopixelColor::Red, &mut channel, &config, &mut led)?;
                 httpd.clear();
             }
             Ok(SysLoopMsg::IpAddressAsquired) => {
                 info!("mpsc loop: IpAddressAsquired received");
 
+                neopixel(NeopixelColor::Green, &mut channel, &config, &mut led)?;
                 let server_config = Configuration::default();
                 let mut s = httpd.create(&server_config);
 
@@ -184,4 +210,33 @@ fn main() -> anyhow::Result<()> {
 
         esp_idf_hal::delay::FreeRtos::delay_ms(100);
     }
+}
+
+fn ns(nanos: u64) -> Duration {
+    Duration::from_nanos(nanos)
+}
+
+fn neopixel(
+    color: NeopixelColor,
+    channel: impl Peripheral<P = impl RmtChannel>,
+    config: &TransmitConfig,
+    led: impl Peripheral<P = impl OutputPin>,
+) -> anyhow::Result<()> {
+    let mut tx = RmtDriver::new(channel, led, &config)?;
+
+    let ticks_hz = tx.counter_clock()?;
+    let t0h = Pulse::new_with_duration(ticks_hz, PinState::High, &ns(350))?;
+    let t0l = Pulse::new_with_duration(ticks_hz, PinState::Low, &ns(800))?;
+    let t1h = Pulse::new_with_duration(ticks_hz, PinState::High, &ns(700))?;
+    let t1l = Pulse::new_with_duration(ticks_hz, PinState::Low, &ns(600))?;
+
+    let mut signal = FixedLengthSignal::<24>::new();
+    for i in 0..24 {
+        let bit = 2_u32.pow(i) & color as u32 != 0;
+        let (high_pulse, low_pulse) = if bit { (t1h, t1l) } else { (t0h, t0l) };
+        signal.set(i as usize, &(high_pulse, low_pulse))?;
+    }
+    tx.start_blocking(&signal)?;
+
+    Ok(())
 }
