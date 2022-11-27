cb5af1f2b5ab1579d8c4d71f0efead11e2717808
Michael Zill <michael.zill@gmail.com>
2022-11-27 22:24:28 +0100

    script updated to new espflash tool

--- create-ota-img.sh
+++ create-ota-img.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 version=$(sed -n 's/^version = //p' Cargo.toml | tr -d '"')
-cargo espflash  save-image --release ESP32-S3 --flash-size 2MB ./bin/firmware-$version.bin
+cargo espflash  save-image --release --chip esp32s3 --flash-size 2M ./bin/firmware-$version.bin
